55c81e9b5fd7f8c9913b8dc3bea90d2b25707b32
khurramHashmi <khurramhashim94@gmail.com>
2020-09-04 15:00:36 +0200

    transformer clean code added with running network for training

---
+++ main.py
@@ -0,0 +1,102 @@
+from skeleton_data_reader import SkeletonsDataset
+from model_transformer import *
+import torch
+import time
+from torch.utils.data import DataLoader, Dataset
+
+def train():
+
+    train_dataset = SkeletonsDataset('data/skel_train.tsv')
+    model.train() # Turn on the train mode
+    total_loss = 0.
+    start_time = time.time()
+    # bptt = 4
+    batch_size = 2
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, **kwargs)
+
+    # for batch, i in enumerate(range(0, 5 - 1, bptt)): # Size will be the number of videos in the sequence
+    batch=200
+    for i in train_loader:
+        data, targets = i
+        data = data.to(device)
+        targets = targets.view(-1).to(device)
+
+        optimizer.zero_grad()
+        output = model(data)
+
+        loss = criterion(output.view(-1, ntokens), targets)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+        #
+        total_loss += loss.item()
+        log_interval = 200
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | '
+                  'lr {:02.2f} | ms/batch {:5.2f} | '
+                  'loss {:5.2f} | ppl {:8.2f}'.format(
+                    epoch, batch, len(data), scheduler.get_lr()[0],
+                    elapsed * 1000 / log_interval,
+                    cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+
+def evaluate(eval_model):
+    eval_model.eval() # Turn on the evaluation mode
+    total_loss = 0.
+
+    eval_dataset = SkeletonsDataset('data/skel_train.tsv')
+    eval_loader = DataLoader(eval_dataset, batch_size=1, shuffle=False, **kwargs)
+    # ntokens = 15400
+    with torch.no_grad():
+        for i in eval_loader:
+            data, targets = i
+            # giving the tensors to Cuda
+            data = data.to(device)
+            targets = targets.view(-1).to(device) # Linearize the target tensor to match the shape
+
+            output = eval_model(data)
+            output_flat = output.view(-1, ntokens)
+            total_loss += len(data) * criterion(output_flat, targets).item()
+    return total_loss / (len(targets) - 1)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if device == 'cuda' else {}
+
+#Defining Model with parameters
+ntokens = 15400 # the size of vocabulary
+emsize = 200 # embedding dimension
+nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 2 # the number of heads in the multiheadattention models
+dropout = 0.2 # the dropout value
+model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
+
+criterion = nn.CrossEntropyLoss()
+lr = 5.0 # learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+best_val_loss = float("inf")
+max_epochs = 10 # number of epochs
+
+for epoch in range(1,  max_epochs + 1):
+    epoch_start_time = time.time()
+    train()
+    val_loss = evaluate(model)
+    print('-' * 89)
+    print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+          'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                     val_loss, math.exp(val_loss)))
+    print('-' * 89)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        best_model = model
+
+    scheduler.step()
